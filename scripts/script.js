d5c989f87a1f54a298f56cdd6bdcea6c55ea4cb5
webc0der <31864367+webc0der@users.noreply.github.com>
2019-03-03 23:39:12 +0530

    js added

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,24 @@
-//project related script goes here.
+var mountainInfo;
+var mountainJSON;
+var avg;
+window.onload= function loadJSON(){
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    console.log('1');
+    xobj.open('GET', 'mountain.json', true);
+    console.log(xobj.status+" "+xobj.readyState);
+    console.log("2");
+    xobj.onreadystatechange= function () {
+      console.log("3");
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            console.log("yes");
+            mountainInfo= xobj.responseText;
+          }
+          else {
+            console.log("no");
+          }
+          xobj.send(null);
+          console.log("4");
+  };
+};
+// mountainJSON= JSON.parse(mountainInfo);
